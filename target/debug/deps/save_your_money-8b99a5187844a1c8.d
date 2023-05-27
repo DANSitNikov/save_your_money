72e24df6b45e0c1fcdd604541dd8c62284f79026
Daniil Sitnikov <best_developer@Daniils-MBP.home>
2023-05-27 19:48:48 +0300

    refactor: remove targets

--- target/debug/deps/save_your_money-8b99a5187844a1c8.d
+++
@@ -1,5 +0,0 @@
-/Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/target/debug/deps/save_your_money-8b99a5187844a1c8: src/main.rs
-
-/Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/target/debug/deps/save_your_money-8b99a5187844a1c8.d: src/main.rs
-
-src/main.rs:
--- target/debug/save_your_money.d
+++
@@ -1,1 +0,0 @@
-/Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/target/debug/save_your_money: /Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/src/main.rs
