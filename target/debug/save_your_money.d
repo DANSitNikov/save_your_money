fab228a7068505a5079dc11eb8bd40284ae116cb
Daniil Sitnikov <best_developer@Daniils-MBP.home>
2023-05-27 19:41:00 +0300

    start project

---
+++ target/debug/save_your_money.d
@@ -0,0 +1,1 @@
+/Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/target/debug/save_your_money: /Users/best_developer/Documents/pet-projects/save-your-money/save_your_money/src/main.rs
